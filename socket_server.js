293d5ee2030b8969b88a1088c5a25b1b35bce4d7
Jaxon Welsh <jaxonwelsh+github@gmail.com>
2016-11-05 01:35:19 -0700

    clean

--- socket_server.js
+++ socket_server.js
@@ -1,73 +1,27 @@
-//
-'use strict';
+var server = require( 'http' ).createServer();
+var url = require( 'url' );
+var ws = require( 'ws' );
+var wsServer = new ws.Server( { server: httpServer } );
+var express = require( 'express' );
+var app = express();
 
-const util = require( 'util' );
-// util.inspect()
+const port = 4080;
 
-const ws = require( 'ws' );
-const WebSocketServer = ws.Server;
-
-// consider using existing server, check docs
-
-const port = 5000;
-const wss = new WebSocketServer( { port: port }, function () {
-	console.log( 'server started on port ' + port );
+app.use( function ( request, response ) {
+	response.send( { message: "hello" } );
 } );
 
-wss.broadcast = function ( data ) {
-	wss.clients.forEach( function ( client ) {
-		client.send( data );
+wsServer.on('connection', function connection( socket ) {
+	var location = url.parse( socket.upgradeReq.url, true );
+	// you might use location.query.access_token to authenticate or share sessions
+	// or ws.upgradeReq.headers.cookie (see http://stackoverflow.com/a/16395220/151312)
+
+	socket.on( 'message', function incoming( message ) {
+		console.log( 'received: %s', message );
 	} );
-};
 
-// wss.handleUpgrade( request, socket, upgradeHead, function( socket ) {
+	socket.send( 'something' );
+} );
 
-let id = 1;
-let count = 0;
-
-wss.on( 'connection', function ( socket ) {
-	socket.id = id++;
-	console.log( 'connection from: %s %s', socket.id, socket.upgradeReq.connection.remoteAddress );
-	wss.broadcast( JSON.stringify( { type: 'message', message: socket.id + ' has joined' } ) );
-	update();
-
-	// console.log( wss.clients );
-	// console.log( 'headers.host: ' + socket.upgradeReq.headers.host );
-	// console.log( '.url: ' + socket.upgradeReq.url );
-
-	socket.on( 'message', function ( message, flags ) {
-		console.log( 'received message from: ', socket.id, message );
-		// console.log( 'with flags: ', flags );
-		const msgObj = JSON.parse( message );
-		if ( msgObj.type == 'click' ) {
-			count++;
-			update();
-		}
-	});
-
-	socket.on( 'close', function () {
-		console.log( 'closed ' + socket.id );
-		wss.broadcast( JSON.stringify( { type: 'message', message: socket.id + ' has left' } ) );
-	} );
-	// socket.send( 'hello client, this is server' );
-});
-
-function update() {
-	const updateObj = {
-		type: 'update',
-		value: count
-	};
-	wss.broadcast( JSON.stringify( updateObj ) );
-}
-
-
-
-// server.close
-
-// socket.close
-// socket.pause, .resume
-// socket.ping, .pong
-// .terminate
-// .stream
-
-// what is websocket mask
+httpServer.on( 'request', app );
+httpServer.listen(port, function () { console.log('Listening on ' + httpServer.address().port) });
