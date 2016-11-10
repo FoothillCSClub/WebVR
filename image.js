e5d4727751c1473082495fa60291fb90e6fc90f0
Jaxon Welsh <jaxonwelsh+github@gmail.com>
2016-11-09 17:01:33 -0800

    cleanup

--- image.js
+++
@@ -1,6 +0,0 @@
-var qs = document.querySelector.bind( document );
-
-// console.log( qs( '#field' ) );
-qs( '#field' ).addEventListener( 'load', () => console.log( 'field load' ) );
-
-// qs( '#field' ).addEventListener( 'loaded', () => console.log( 'field loaded' ) );
